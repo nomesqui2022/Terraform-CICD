5b0c921e4d5619489d07cd6f264f58c53111def0
Juan Alex 92 <97132342+nomesqui2022@users.noreply.github.com>
2026-03-13 18:55:10 -0500

    Update main.tf

--- cicd/main.tf
+++ cicd/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_container" "example" {
-  name                  = "johnalexander"
+  name                  = "platzibog"
   storage_account_id  = azurerm_storage_account.storage.id 
   container_access_type = "container"
 }
